c9820226a4b3b4d537e40f5833a41b9db212aa11
madouh <tin.tin.nagi@gmail.com>
2016-09-03 17:59:46 +0200

    first commit

---
+++ admin/language/arabic/sale/recurring.php
@@ -0,0 +1,54 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Heading
+$_['heading_title']                        = 'المدفوعات الدورية';
+
+// Text
+$_['text_success']                         = 'تم التعديل !';
+$_['text_list']                            = 'قائمة';
+$_['text_add']                             = 'ادراج';
+$_['text_edit']                            = 'تحرير';
+$_['text_payment_profiles']                = 'المدفوعات الدورية';
+$_['text_status_active']                   = 'مفعل';
+$_['text_status_inactive']                 = 'غير مفعل';
+$_['text_status_cancelled']                = 'ملغي';
+$_['text_status_suspended']                = 'موقوف';
+$_['text_status_expired']                  = 'انتهى';
+$_['text_status_pending']                  = 'معلق';
+$_['text_transactions']                    = 'الدفعات';
+$_['text_cancel_confirm']                  = 'الغاء المدفوعات الدورية لايمكن استرجاعها لاحقا ! هل ترغب تأكيد الالغاء ؟';
+$_['text_transaction_date_added']          = 'تاريخ البداية';
+$_['text_transaction_payment'] 			   = 'الدفع';
+$_['text_transaction_outstanding_payment'] = 'الدفعات المستحقة';
+$_['text_transaction_skipped']             = 'دفعات تم تجاهلها';
+$_['text_transaction_failed']              = 'دفعات فشلت';
+$_['text_transaction_cancelled']           = 'دفعات ملغية';
+$_['text_transaction_suspended']           = 'دفعات موقوفة';
+$_['text_transaction_suspended_failed']    = 'موقوفة بسبب فشل الدفع';
+$_['text_transaction_outstanding_failed']  = 'دفعات مستحقة فشلت';
+$_['text_transaction_expired']             = 'انتهت';
+
+// Entry
+$_['entry_cancel_payment']                 = 'الغاء الدفع';
+$_['entry_order_recurring']                = 'الرقم';
+$_['entry_order_id']                       = 'رقم الطلب';
+$_['entry_reference']                      = 'طريقة الدفع';
+$_['entry_customer']                       = 'اسم العميل';
+$_['entry_date_added']                     = 'تاريخ الطلب';
+$_['entry_status']                         = 'حالة الطلب';
+$_['entry_type']                           = 'النوع';
+$_['entry_action']                         = 'تحرير';
+$_['entry_email']                          = 'البريد الالكتروني';
+$_['entry_description']                    = 'وصف المدفوعات الدورية';
+$_['entry_product']                        = 'المنتجات';
+$_['entry_quantity']                       = 'الكمية';
+$_['entry_amount']                         = 'المبلغ';
+$_['entry_recurring']                      = 'المدفوعات الدورية';
+$_['entry_payment_method']                 = 'طريقة الدفع';
+
+// Error / Success
+$_['error_not_cancelled']                  = 'خطأ: %s';
+$_['error_not_found']                      = 'لا يمكن الغاء المدفوعات الدورية';
+$_['text_cancelled']                       = 'تم الغاء المدفوعات الدورية';
---
+++ admin/language/arabic/tool/backup.php
@@ -0,0 +1,20 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Heading
+$_['heading_title']    = 'نسخة احتياطية / استعادة';
+
+// Text
+$_['text_backup']      = 'عمل نسخة احتياطية';
+$_['text_success']     = 'تم تصدير قاعدة البيانات بنجاح !';
+$_['text_list']        = 'قائمة';
+
+// Entry
+$_['entry_restore']    = 'إستعادة نسخة احتياطية :';
+$_['entry_backup']     = 'تصدير نسخة احتياطية :';
+
+// Error
+$_['error_permission'] = 'تحذير : أنت لا تمتلك صلاحيات التعديل !';
+$_['error_backup']     = 'تحذير : يجب اختيار جدول واحد على الأقل !';
+$_['error_empty']      = 'تحذير : الملف الذي قمت باختياره فارغ !';
